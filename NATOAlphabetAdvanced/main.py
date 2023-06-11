26ca0421600b4f442b2ed68c80063bffc997a972
Beena <beenabhagchandani@gmail.com>
2023-06-11 17:11:48 +0530

    fixing changes

--- NATOAlphabetAdvanced/main.py
+++ NATOAlphabetAdvanced/main.py
@@ -1,15 +1,18 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+is_string = False
+
 #TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+while not is_string:
+	word = input("Enter a word: ").upper()
+	try:
+		output_list = [phonetic_dict[letter] for letter in word]		
+	except KeyError:
+		print("Please enter only alphabet charcter")
+	else:
+		print(output_list)
+		is_string = True
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
